5561472f05d668882833acf2d34af869cfacdf2a
Rusewww <rusewww1@gmail.com>
2022-01-12 23:12:40 +0200

    fix

--- src/mx_replace_substitution.c
+++ src/mx_replace_substitution.c
@@ -1,8 +1,7 @@
 #include "ush.h"
 
-static void get_quoted_sub(int *o_sub, int *c_sub,
-                           unsigned int *i, char *arg) {
-    while (arg[*i]) {
+static void get_quoted_sub(int *o_sub, int *c_sub, unsigned int *i, char *arg) {
+    for (; arg[*i]; *i += 1) {
         mx_skip_quotes(arg, i, MX_S_QUOTES);
         if (arg[*i] == MX_GRAVE_ACCENT
             && !mx_isescape_char(arg, *i) && *o_sub == -1) {
@@ -15,7 +14,6 @@ static void get_quoted_sub(int *o_sub, int *c_sub,
             *i += 1;
             break;
         }
-        *i += 1;
     }
 }
 
@@ -24,11 +22,9 @@ static void inc_subs(int *sub, unsigned int *i, int add) {
     *i += add;
 }
 
-static void get_parameter_sub(int *o_sub, int *c_sub,
-                              unsigned int *i, char *arg) {
+static void get_parameter_sub(int *o_sub, int *c_sub, unsigned int *i, char *arg) {
     unsigned int brace = 0;
-
-    while (arg[*i]) {
+    for (; arg[*i]; *i += 1) {
         mx_skip_quotes(arg, i, MX_S_QUOTES);
         if (arg[*i] == '$' && arg[*i + 1] == '('
             && !mx_isescape_char(arg, *i) && *o_sub == -1) {
@@ -44,42 +40,43 @@ static void get_parameter_sub(int *o_sub, int *c_sub,
                 break;
             }
         }
-        *i += 1;
     }
 }
 
-static char *get_substitution(int *o_sub, int *c_sub,
-                              unsigned int *i, char *arg) {
+static char *get_substitution(int *o_sub, int *c_sub, unsigned int *i, char *arg) {
     if (arg[*i] == MX_GRAVE_ACCENT && !mx_isescape_char(arg, *i)) {
         get_quoted_sub(o_sub, c_sub, i, arg);
-    }
-    else if (arg[*i] == '$' && !mx_isescape_char(arg, *i)) {
+    } else if (arg[*i] == '$' && !mx_isescape_char(arg, *i)) {
         get_parameter_sub(o_sub, c_sub, i, arg);
     }
-    if (*o_sub != -1 && *c_sub != -1)
+    if (*o_sub != -1 && *c_sub != -1) {
         return strndup(arg + *o_sub, *c_sub - *o_sub + 1);
+    }
     return NULL;
 }
 
 char *mx_replace_substitution(char *arg, int *code) {
     int i_s[3] = {-1, -1, 0};
-    bool is_quotes = false;
+    bool quote = false;
     char *sub = NULL;
-    char *result = mx_strnew(ARG_MAX);
-
-    for (unsigned int i = 0; i < strlen(arg); i++, i_s[0] = -1, i_s[1] = -1) {
-        mx_skip_quotes_if(&is_quotes, arg, &i);
+    char *res = mx_strnew(ARG_MAX);
+    unsigned int i = 0;
+    while (i < strlen(arg)) {
+        mx_skip_quotes_if(&quote, arg, &i);
         if ((sub = get_substitution(&i_s[0], &i_s[1], &i, arg))) {
-            strncat(result, arg + i_s[2], i_s[0] - i_s[2]);
-            if (!mx_get_sub(result, sub, code))
-                return mx_get_invalid_sub(&arg, &result, &sub);
+            strncat(res, arg + i_s[2], i_s[0] - i_s[2]);
+            if (!mx_get_sub(res, sub, code))
+                return mx_get_invalid_sub(&arg, &res, &sub);
             i = i_s[1];
             i_s[2] = i + 1;
-        }
-        else if (i_s[0] != -1 && i_s[1] == -1 && !sub)
+        } else if (i_s[0] != -1 && i_s[1] == -1 && !sub) {
             break;
+        }
+        i++;
+        i_s[0] = -1;
+        i_s[1] = -1;
     }
-    strcat(result, arg + i_s[2]);
+    strcat(res, arg + i_s[2]);
     mx_strdel(&arg);
-    return result;
+    return res;
 }
