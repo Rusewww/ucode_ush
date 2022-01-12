51089ac882c6696cf25220e9606a10aef7a149a7
Rusewww <rusewww1@gmail.com>
2022-01-12 19:34:19 +0200

    env

--- src/mx_env_parse_vars.c
+++ src/mx_env_parse_vars.c
@@ -1,24 +1,27 @@
 #include "ush.h"
 
 void mx_env_parse_vars(char **argv, char **path, int *idx) {
-    char *var_name = NULL;
-    bool path_replace = 0;
+    char *name = NULL;
+    bool replace = false;
 
-    for (; argv[*idx]; (*idx)++) {
+    while (argv[*idx]) {
         if (mx_match(argv[*idx], MX_ENV_VAR)) {
-            mx_get_name(argv[*idx], &var_name);
+            mx_get_name(argv[*idx], &name);
             mx_putenv(argv[*idx]);
-            if (!strcmp(var_name, "PATH") && (!(*path) || path_replace)) {
-                path_replace = 1;
-                if (*path)
+            if (!strcmp(name, "PATH") && (!(*path) || replace)) {
+                replace = true;
+                if (*path) {
                     mx_strdel(path);
+                }
                 *path = mx_get_var_info(argv[*idx], 1);
             }
-        }
-        else
+        } else {
             break;
+        }
+        (*idx)++;
     }
-    if (!(*path) && mx_get_var_val(SHELL, "PATH"))
+    if (!(*path) && mx_get_var_val(SHELL, "PATH")) {
         *path = strdup(mx_get_var_val(SHELL, "PATH"));
-    mx_strdel(&var_name);
+    }
+    mx_strdel(&name);
 }
