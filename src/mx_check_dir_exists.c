8817685e84428eb71411b616dceacb983caebe59
Rusewww <rusewww1@gmail.com>
2022-01-12 17:31:20 +0200

    fix

--- src/mx_check_dir_exists.c
+++ src/mx_check_dir_exists.c
@@ -1,9 +1,10 @@
 #include "ush.h"
 
 bool mx_check_dir_exists(char *dir) {
-    if (chdir(dir) < 0)
+    if (chdir(dir) < 0) {
         return false;
-    else
+    } else {
         chdir("-");
+    }
     return true;
 }
