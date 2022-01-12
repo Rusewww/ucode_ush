e5cab06d94590ad69b2e25dbec6d60549c0e65bc
Rusewww <rusewww1@gmail.com>
2022-01-12 18:02:45 +0200

    subs

--- src/mx_clear_slash_end.c
+++ src/mx_clear_slash_end.c
@@ -3,10 +3,9 @@
 char *mx_clear_slash_end(char *str) {
     if (str[mx_strlen(str) - 1] == '/' && mx_strlen(str) > 1) {
         char *result = mx_strndup(str, mx_strlen(str) - 1);
-
         mx_strdel(&str);
         return result;
-    }
-    else
+    } else {
         return str;
+    }
 }
