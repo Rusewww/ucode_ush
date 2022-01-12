2bdcb2d60b69351c588b4b5fc6282e3147cfd401
Rusewww <rusewww1@gmail.com>
2022-01-12 23:54:31 +0200

    fix

--- src/mx_replace_escape.c
+++ src/mx_replace_escape.c
@@ -1,21 +1,22 @@
 #include "ush.h"
 
-char *mx_replace_escape(char *arg, char *esc, char new, bool in_q) {
-    bool q = false;
-    char *res = mx_strnew(ARG_MAX);
+char *mx_replace_escape(char *arg, char *escape, char new, bool in_q) {
+    bool d_q = false;
+    char *result = mx_strnew(ARG_MAX);
     unsigned int index = 0;
-    unsigned int i = 0;
-    while (i < strlen(arg)) {
-        if (arg[i] == MX_D_QUOTES && !mx_isescape_char(arg, i)) {
-            q = !q;
-        }
-        if (arg[i] == esc[0] && arg[i + 1] == esc[1] && mx_isescape_char(arg, i + 1) && (q || in_q)) {
+
+    for (unsigned int i = 0; i < strlen(arg); i++) {
+        if (arg[i] == MX_D_QUOTES && !mx_isescape_char(arg, i))
+            d_q = !d_q;
+        if (arg[i] == escape[0]
+            && arg[i + 1] == escape[1]
+            && mx_isescape_char(arg, i + 1)
+            && (d_q || in_q)) {
             i++;
             arg[i] = new;
         }
-        res[index++] = arg[i];
-        i++;
+        result[index++] = arg[i];
     }
     mx_strdel(&arg);
-    return res;
+    return result;
 }
