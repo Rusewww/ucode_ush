4870a0b58775eca44711e68e3297d3a33ae6d3fa
Rusewww <rusewww1@gmail.com>
2022-01-12 17:54:58 +0200

    fix

--- src/mx_check_s_slash.c
+++ src/mx_check_s_slash.c
@@ -1,15 +1,14 @@
 #include "ush.h"
 
-void mx_check_s_slash(char *newdir, t_map **map) {
-    char *str = realpath(newdir, NULL);
-    char *newstr = mx_parse_path(NULL, newdir, map);
+void mx_check_s_slash(char *dir, t_map **map) {
+    char *str = realpath(dir, NULL);
+    char *str_new = mx_parse_path(NULL, dir, map);
 
-    if (strcmp(newstr, str) == 0) {
-        mx_change_dir(newdir, map, 1);
+    if (strcmp(str_new, str) == 0) {
+        mx_change_dir(dir, map, 1);
         mx_strdel(&str);
-    }
-    else {
-        fprintf(stderr, "cd: %s: is not a directory.\n", newdir);
+    } else {
+        fprintf(stderr, "cd: %s: is not a directory.\n", dir);
         mx_strdel(&str);
         str = mx_get_map(map, "PWD");
         chdir("/");
