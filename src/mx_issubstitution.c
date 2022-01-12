4815dfd47985cc05358cf34080efa13260e1ad35
Rusewww <rusewww1@gmail.com>
2022-01-12 22:26:38 +0200

    handle

--- src/mx_issubstitution.c
+++ src/mx_issubstitution.c
@@ -3,12 +3,11 @@
 bool mx_issubstitution(char *arg) {
     unsigned int length = strlen(arg);
     bool res = false;
-
     if (arg[0] == '`' && arg[length - 1] == '`') {
         res = true;
     }
     if (arg[0] == '$' && arg[1] == '(' && arg[length - 1] == ')') {
         res = true;
-
-        return res;
     }
+    return res;
+}
