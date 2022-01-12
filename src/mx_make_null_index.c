f04e37a7d02d03bc76a87dd6f4ceb585bb5b875f
Rusewww <rusewww1@gmail.com>
2022-01-13 00:37:03 +0200

    1

--- src/mx_make_null_index.c
+++ src/mx_make_null_index.c
@@ -3,14 +3,11 @@
 char **mx_make_null_index(char **split, int index) {
     mx_strdel(&split[index]);
     split[index] = strdup("");
-    int i = index - 1;
-    while (split[i] && i > -1) {
+    for (int i = index - 1; split[i] && i > -1; i--)
         if (strcmp(split[i], "") != 0) {
             mx_strdel(&split[i]);
             split[i] = strdup("");
             break;
         }
-        i--;
-    }
     return split;
 }
