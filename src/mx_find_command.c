4ec781e75667177e12d201f555a3d666a3939a77
Rusewww <rusewww1@gmail.com>
2022-01-12 20:58:18 +0200

    fq

--- src/mx_find_command.c
+++ src/mx_find_command.c
@@ -3,45 +3,50 @@
 static bool check_dir(char *path, char *file) {
     DIR *dir = opendir(path);
     struct dirent *entry;
-    bool retval = 0;
+    bool rval = 0;
 
     if (dir) {
         while ((entry = readdir(dir)) != NULL)
-            if (!strcmp(entry->d_name, file) &&
-                strcmp(entry->d_name, ".") && strcmp(entry->d_name, ".."))
-                retval = 1;
+            if (!strcmp(entry->d_name, file) && strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")) {
+                rval = 1;
+            }
         closedir(dir);
     }
-    return retval;
+    return rval;
 }
 
-static void check_in_abspath(char *command, char **filename, bool *retval) {
-    char *dir_name = dirname(command);
-    char *base_name = basename(command);
+static void check_in_abspath(char *cmnd, char **name, bool *rval) {
+    char *d_name = dirname(cmnd);
+    char *b_name = basename(cmnd);
 
-    if (check_dir(dir_name, base_name)) {
-        *retval = 1;
-        *filename = mx_strdup(command);
+    if (check_dir(d_name, b_name)) {
+        *rval = 1;
+        *name = mx_strdup(cmnd);
     }
 }
 
-bool mx_find_command(char *path, char *command, char **filename) {
-    char **paths = mx_strsplit(path, ':');
-    bool retval = 0;
+bool mx_find_command(char *path, char *cmnd, char **filen) {
+    char **path = mx_strsplit(path, ':');
+    bool rval = 0;
     char fname[PATH_MAX];
 
-    if (paths) {
-        for (int i = 0; paths[i]; i++)
-            if (check_dir(paths[i], command)) {
-                retval = 1;
-                sprintf(fname, "%s/%s", paths[i], command);
-                if (*filename)
-                    mx_strdel(filename);
-                *filename = strdup(fname);
+    if (path) {
+        int i = 0;
+        while (path[i]) {
+            if (check_dir(path[i], cmnd)) {
+                rval = 1;
+                sprintf(fname, "%s/%s", path[i], cmnd);
+                if (*filen) {
+                    mx_strdel(filen);
+                }
+                *filen = strdup(fname);
             }
-        mx_del_strarr(&paths);
+            mx_del_strarr(&path);
+            i++;
+        }
     }
-    if (!retval)
-        check_in_abspath(command, filename, &retval);
-    return retval;
+    if (!rval) {
+        check_in_abspath(cmnd, filen, &rval);
+    }
+    return rval;
 }
