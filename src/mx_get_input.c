bbc43baee27f6df6a668415f9da634de8c28f469
Rusewww <rusewww1@gmail.com>
2022-01-12 21:09:29 +0200

    fq

--- src/mx_get_input.c
+++ src/mx_get_input.c
@@ -1,20 +1,23 @@
 #include "ush.h"
 
-static void clear_prompt(t_prompt *prompt) {
+/*static void clear_prompt(t_prompt *prompt) {
     memset(prompt->command, '\0', sizeof(prompt->command));
     memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
     prompt->index = 0;
     prompt->cursor_index = 0;
     prompt->end = false;
-}
+}*/
 
 static void set_code(t_prompt *prompt, int *code) {
     if (prompt->buff[0] == '\x03' && strlen(prompt->buff) == 1) {
-        clear_prompt(prompt);
+        memset(prompt->command, '\0', sizeof(prompt->command));
+        memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
+        prompt->index = 0;
+        prompt->cursor_index = 0;
+        prompt->end = false;
         *code = 130;
     }
-    if (prompt->buff[0] == '\x04'
-        && !prompt->index && strlen(prompt->buff) == 1) {
+    if (prompt->buff[0] == '\x04' && !prompt->index && strlen(prompt->buff) == 1) {
         *code = -1;
     }
 }
@@ -27,10 +30,12 @@ static void clear_view(t_prompt *prompt, int fd) {
 
 static bool handle_key(t_prompt *prompt, int fd, int *code) {
     set_code(prompt, code);
-    if (mx_match(prompt->buff, MX_NEW_LINE_CHARS))
+    if (mx_match(prompt->buff, MX_NEW_LINE_CHARS)) {
         return false;
-    if (prompt->buff[0] == '\x04' && !prompt->index)
+    }
+    if (prompt->buff[0] == '\x04' && !prompt->index) {
         return false;
+    }
     clear_view(prompt, fd);
     if (!mx_handle_history(prompt)) {
         mx_handle_print_char(prompt);
@@ -40,11 +45,13 @@ static bool handle_key(t_prompt *prompt, int fd, int *code) {
 }
 
 void mx_get_input(t_prompt *prompt, int fd, int *code) {
-    clear_prompt(prompt);
+    memset(prompt->command, '\0', sizeof(prompt->command));
+    memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
+    prompt->index = 0;
+    prompt->cursor_index = 0;
+    prompt->end = false;
     dprintf(fd, "%s%s", mx_str_prompt(), prompt->command);
-    while (read(STDIN_FILENO, prompt->buff, 4)
-           && prompt->index < ARG_MAX
-           && handle_key(prompt, fd, code)) {
+    while (read(STDIN_FILENO, prompt->buff, 4) && prompt->index < ARG_MAX && handle_key(prompt, fd, code)) {
         dprintf(fd, "%s%s", mx_str_prompt(), prompt->command);
         mx_handle_cursor(prompt);
         memset(prompt->buff, '\0', sizeof(prompt->buff));
