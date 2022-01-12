1773c28908d53f54f11ca0e9d76d22cf1ad39541
Rusewww <rusewww1@gmail.com>
2022-01-13 00:22:48 +0200

    fix

--- src/mx_preinterpretate.c
+++ src/mx_preinterpretate.c
@@ -1,23 +1,20 @@
 #include "ush.h"
 
 int mx_preinterpretate(char *command) {
-    int result = 0;
+    int res = 0;
 
-    if (!(result = mx_check_quotes(command))) {
-        fprintf(stderr, "%s: missing terminating character\n", 
-        MX_SHELL_NAME);
-    }
-    else if (!(result = mx_check_brackets(command))) {
-        fprintf(stderr, "%s: missing brace character\n", 
-        MX_SHELL_NAME);
-    }
-    else if (!(result = mx_check_substitutions(command))) {
-        fprintf(stderr, "%s: missing subsitution character\n", 
-        MX_SHELL_NAME);
-    }
-    else if (!(result = mx_check_chars(command))) {
+    if (!(res = mx_check_quotes(command))) {
+        fprintf(stderr, "%s: missing terminating character\n",
+                MX_SHELL_NAME);
+    } else if (!(res = mx_check_brackets(command))) {
+        fprintf(stderr, "%s: missing brace character\n",
+                MX_SHELL_NAME);
+    } else if (!(res = mx_check_substitutions(command))) {
+        fprintf(stderr, "%s: missing subsitution character\n",
+                MX_SHELL_NAME);
+    } else if (!(res = mx_check_chars(command))) {
         fprintf(stderr, "%s: command contain forbidden character\n",
-        MX_SHELL_NAME);
+                MX_SHELL_NAME);
     }
-    return !result;
+    return !res;
 }
