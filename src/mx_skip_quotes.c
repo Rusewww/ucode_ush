81668d765e3f18f7424bec7ad1b923e1c88689f8
Rusewww <rusewww1@gmail.com>
2022-01-12 23:37:22 +0200

    fix

--- src/mx_skip_quotes.c
+++ src/mx_skip_quotes.c
@@ -1,24 +1,30 @@
 #include "ush.h"
 
+static void skip_single_quotes(char *command, unsigned int *i) {
+    if (command[*i] == MX_S_QUOTES && !mx_isescape_char(command, *i)) {
+        (*i)++;
+        while (command[*i]) {
+            if (command[*i] == MX_S_QUOTES) {
+                break;
+            }
+            (*i)++;
+        }
+    }
+}
+
 void mx_skip_quotes(char *command, unsigned int *i, char c) {
     if (c == MX_S_QUOTES) {
-        if (command[*i] == MX_S_QUOTES && !mx_isescape_char(command, *i)) {
-            (*i)++;
-            for (; command[*i]; (*i)++) {
-                if (command[*i] == MX_S_QUOTES) {
-                    break;
-                }
-            }
-        }
+        skip_single_quotes(command, i);
         return;
     }
     if (command[*i] == c && !mx_isescape_char(command, *i)) {
         (*i)++;
-        for (; command[*i]; (*i)++) {
+        while (command[*i]) {
             if (command[*i] == c
                 && !mx_isescape_char(command, *i)) {
                 break;
             }
+            (*i)++;
         }
     }
 }
