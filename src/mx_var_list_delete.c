b540ede647d964f00861662981a5556d6ce30816
Rusewww <rusewww1@gmail.com>
2022-01-12 23:50:25 +0200

    fix

--- src/mx_var_list_delete.c
+++ src/mx_var_list_delete.c
@@ -4,30 +4,31 @@ static void pop_curr_node(t_list **list, t_list **prev, t_list **current) {
     if (*prev == NULL) {
         *prev = (*current)->next;
         *list = *prev;
-    }
-    else
+    } else {
         (*prev)->next = (*current)->next;
+    }
     free((*current)->data);
     free(*current);
     *current = NULL;
 }
 
-void mx_var_list_delete(t_var_list key, char *del_name) {
+void mx_var_list_delete(t_var_list key, char *del_n) {
     t_list **list = mx_get_var_list(key);
-    t_list *current = *list;
+    t_list *curr = *list;
     t_list *prev = NULL;
     char *curr_name = NULL;
 
-    while (current) {
-        mx_get_name(current->data, &curr_name);
-        if (strcmp(del_name, curr_name) == 0) {
-            pop_curr_node(list, &prev, &current);
+    while (curr) {
+        mx_get_name(curr->data, &curr_name);
+        if (strcmp(del_n, curr_name) == 0) {
+            pop_curr_node(list, &prev, &curr);
             break;
         }
-        prev = current;
-        current = current->next;
-        if (current)
-            mx_get_name(current->data, &curr_name);
+        prev = curr;
+        curr = curr->next;
+        if (curr) {
+            mx_get_name(curr->data, &curr_name);
+        }
     }
     mx_strdel(&curr_name);
 }
