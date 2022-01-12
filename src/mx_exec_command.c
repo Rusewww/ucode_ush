78ce354cab1a9ba2588e99754d72e483fe2ddb73
Rusewww <rusewww1@gmail.com>
2022-01-12 20:01:23 +0200

    env

--- src/mx_exec_command.c
+++ src/mx_exec_command.c
@@ -1,66 +1,86 @@
 #include "ush.h"
 
 static int exec_builtin2(char *command, char **argv, int fd) {
-    if (!strcmp(command, "pwd")) { //
+    if (!strcmp(command, "pwd")) {
         return mx_pwd(&argv[1], fd);
     }
-    if (!strcmp(command, "cd")) { //
+    if (!strcmp(command, "cd")) {
         return mx_cd(&argv[1], fd);
     }
-    if (!strcmp(command, "echo")) { //
+    if (!strcmp(command, "echo")) {
         return mx_echo(&argv[1], fd);
     }
-    if (!strcmp(command, "env")) { //
+    if (!strcmp(command, "env")) {
         return mx_env(&argv[1], fd);
     }
-    if (!strcmp(command, "fg")) { //
+    if (!strcmp(command, "fg")) {
         return mx_fg(&argv[1], fd);
+    }
+    if (!strcmp(command, "jobs")) {
+        return mx_jobs(&argv[1], fd);
+    }
+    if (!strcmp(command, "set")) {
+        return mx_set(&argv[1], fd);
+    }
+    if (!strcmp(command, "chdir")) {
+        return mx_chdir(&argv[1], fd);
+    }
+    if (!strcmp(command, "bye")) {
+        return mx_bye(&argv[1]);
     }
     return 1;
 }
 
 static int exec_builtin1(char *command, char **argv, int fd) {
-    if (!strcmp(command, "export")) { //
+    if (!strcmp(command, "export")) {
         return mx_export(&argv[1], fd);
     }
-    if (!strcmp(command, "which")) { //
+    if (!strcmp(command, "which")) {
         return mx_which(&argv[1], fd);
     }
-    if (!strcmp(command, "unset")) { //
+    if (!strcmp(command, "unset")) {
         return mx_unset(&argv[1]);
     }
     if (!strcmp(command, "color")) {
         return mx_color(&argv[1]);
     }
-    if (!strcmp(command, "exit")) { //
+    if (!strcmp(command, "false")) {
+        return mx_false();
+    }
+    if (!strcmp(command, "true")) {
+        return mx_true();
+    }
+    if (!strcmp(command, "exit")) {
         return mx_exit(&argv[1]);
     }
     return exec_builtin2(command, argv, fd);
 }
 
-static int print_exec_error(char *error) {
-    if (mx_match(error, "/")) {
-        fprintf(stderr, "%s: %s: No such file or directory\n", MX_SHELL_NAME, error);
+static int print_exec_error(char *error_name) {
+    if (mx_match(error_name, "/")) {
+        fprintf(stderr, "%s: %s: No such file or directory\n", MX_SHELL_NAME, error_name);
     } else {
-        fprintf(stderr, "%s: %s: Command not found\n", MX_SHELL_NAME, error);
+        fprintf(stderr, "%s: %s: Command not found\n", MX_SHELL_NAME, error_name);
     }
     return 127;
 }
 
 int mx_exec_command(char **argv, int fd) {
-    char *name = NULL;
-    int rval = 0;
+    char *filename = NULL;
+    int retval = 0;
 
     if (mx_is_builtin(argv[0])) {
         return exec_builtin1(argv[0], argv, fd);
-    } else if (mx_find_command(mx_get_var_val(SHELL, "PATH"), argv[0], &name)) {
+    } else if (mx_find_command(mx_get_var_val(SHELL, "PATH"), argv[0], &filename)) {
         extern char **environ;
         t_process *process = mx_create_process(fd);
-        rval = mx_exec(process, name, argv, environ);
+
+        retval = mx_exec(process, filename, argv, environ);
         mx_del_process(&process);
-        mx_strdel(&name);
+        mx_strdel(&filename);
     } else {
-        rval = print_exec_error(argv[0]);
+        retval = print_exec_error(argv[0]);
+
     }
-    return rval;
+    return retval;
 }
