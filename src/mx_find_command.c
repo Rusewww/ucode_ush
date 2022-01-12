26ec703093b06361287a1fc76f5585a71555c75f
Rusewww <rusewww1@gmail.com>
2022-01-12 21:02:17 +0200

    fq

--- src/mx_find_command.c
+++ src/mx_find_command.c
@@ -4,49 +4,49 @@ static bool check_dir(char *path, char *file) {
     DIR *dir = opendir(path);
     struct dirent *entry;
     bool rval = 0;
-
     if (dir) {
-        while ((entry = readdir(dir)) != NULL)
+        while ((entry = readdir(dir)) != NULL) {
             if (!strcmp(entry->d_name, file) && strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")) {
                 rval = 1;
             }
+        }
         closedir(dir);
     }
     return rval;
 }
 
-static void check_in_abspath(char *cmnd, char **name, bool *rval) {
-    char *d_name = dirname(cmnd);
-    char *b_name = basename(cmnd);
+static void check_in_abspath(char *command, char **name, bool *rval) {
+    char *d_name = dirname(command);
+    char *b_name = basename(command);
 
     if (check_dir(d_name, b_name)) {
         *rval = 1;
-        *name = mx_strdup(cmnd);
+        *name = mx_strdup(command);
     }
 }
 
-bool mx_find_command(char *path, char *cmnd, char **filen) {
+bool mx_find_command(char *path, char *command, char **name) {
     char **path = mx_strsplit(path, ':');
-    bool rval = 0;
+    bool retval = 0;
     char fname[PATH_MAX];
 
     if (path) {
         int i = 0;
         while (path[i]) {
-            if (check_dir(path[i], cmnd)) {
-                rval = 1;
-                sprintf(fname, "%s/%s", path[i], cmnd);
-                if (*filen) {
-                    mx_strdel(filen);
+            if (check_dir(path[i], command)) {
+                retval = 1;
+                sprintf(fname, "%s/%s", path[i], command);
+                if (*name) {
+                    mx_strdel(name);
                 }
-                *filen = strdup(fname);
+                *name = strdup(fname);
             }
-            mx_del_strarr(&path);
             i++;
         }
+        mx_del_strarr(&path);
     }
-    if (!rval) {
-        check_in_abspath(cmnd, filen, &rval);
+    if (!retval) {
+        check_in_abspath(command, name, &retval);
     }
-    return rval;
+    return retval;
 }
