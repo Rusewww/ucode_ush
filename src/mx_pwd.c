cb988a3ff975a0a70f44c7f52dc1f50f897164e1
Rusewww <rusewww1@gmail.com>
2022-01-13 00:19:21 +0200

    fix

--- src/mx_pwd.c
+++ src/mx_pwd.c
@@ -1,24 +1,27 @@
 #include "ush.h"
 
 static bool parse_flags(char **flags, bool *mode) {
-    bool flag_stop = 0;
+    bool stop_fl = 0;
+    int i = 0;
 
-    for (int i = 0; flags[i]; i++) {
-        if (flags[i][0] == '-' && !flag_stop) {
-            flag_stop = mx_is_flag_stop(flags[i]);
-            for (int j = 1; flags[i][j] != '\0'; j++)
-                if (flags[i][j] == 'P')
+    while (flags[i]) {
+        if (flags[i][0] == '-' && !stop_fl) {
+            stop_fl = mx_is_flag_stop(flags[i]);
+            int j = 1;
+            while (flags[i][j] != '\0') {
+                if (flags[i][j] == 'P') {
                     *mode = 1;
-                else if (flags[i][j] != 'L' && (flags[i][j] != '-'
-                         || (flags[i][j] == '-' && j == 2))) {
+                } else if (flags[i][j] != 'L' && (flags[i][j] != '-' || (flags[i][j] == '-' && j == 2))) {
                     fprintf(stderr, "pwd: bad option: -%c\n", flags[i][j]);
                     return 1;
                 }
-        }
-        else {
+                j++;
+            }
+        } else {
             fprintf(stderr, "pwd: too many arguments\n");
             return 1;
         }
+        i++;
     }
     return 0;
 }
@@ -27,16 +30,14 @@ int mx_pwd(char **flags, int fd) {
     t_map **map = mx_get_lenv();
     bool mode = 0;
     char *pwd = mx_get_map(map, "PWD");
-
     if (!parse_flags(flags, &mode)) {
         if (mode) {
             char *rpath = realpath(pwd, NULL);
-
             dprintf(fd, "%s\n", rpath);
             mx_strdel(&rpath);
-        }
-        else
+        } else {
             dprintf(fd, "%s\n", pwd);
+        }
         return 0;
     }
     return 1;
--- src/mx_remove_subchar.c
+++ src/mx_remove_subchar.c
@@ -1,17 +1,16 @@
 #include "ush.h"
 
-bool mx_remove_subchar(char *substitution) {
-    unsigned int len = strlen(substitution);
-
-    if (substitution[0] == '`' && substitution[len - 1] == '`') {
-        substitution[len - 1] = '\0';
-        memmove(substitution, substitution + 1, len - 1);
+bool mx_remove_subchar(char *subs) {
+    unsigned int length = strlen(subs);
+    if (subs[0] == '`' && subs[length - 1] == '`') {
+        subs[length - 1] = '\0';
+        memmove(subs, subs + 1, length - 1);
         return true;
     }
-    if (substitution[0] == '$' && substitution[1] == '('
-        && substitution[len - 1] == ')') {
-        substitution[len - 1] = '\0';
-        memmove(substitution, substitution + 2, len - 2);
+    if (subs[0] == '$' && subs[1] == '('
+        && subs[length - 1] == ')') {
+        subs[length - 1] = '\0';
+        memmove(subs, subs + 2, length - 2);
         return true;
     }
     return false;
