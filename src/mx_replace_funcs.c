3513ae56fc1461fbc15c65229fd284a5762a0168
Rusewww <rusewww1@gmail.com>
2022-01-12 23:00:12 +0200

    fix

--- src/mx_replace_funcs.c
+++ src/mx_replace_funcs.c
@@ -1,43 +1,46 @@
 #include "ush.h"
 
-void mx_skip_exps_quotes(char *arg, unsigned int *index, bool *is_quotes) {
-    if (arg[*index] == MX_D_QUOTES && !mx_isescape_char(arg, *index))
-        *is_quotes = !*is_quotes;
-    if (!*is_quotes)
+void mx_skip_exps_quotes(char *arg, unsigned int *index, bool *quote) {
+    if (arg[*index] == MX_D_QUOTES && !mx_isescape_char(arg, *index)) {
+        *quote = !*quote;
+    }
+    if (!*quote) {
         mx_skip_quotes(arg, index, MX_S_QUOTES);
+    }
     mx_skip_quotes(arg, index, MX_GRAVE_ACCENT);
     mx_skip_expansion(arg, index);
 }
 
-bool mx_replace_env_var(char *result, char *env, unsigned int *index,
-                        unsigned int len) {
+bool mx_replace_env_var(char *res, char *env, unsigned int *index, unsigned int length) {
     if (!env) {
         return false;
     }
-    strcat(result, env);
-    index[0] += len;
+    strcat(res, env);
+    index[0] += length;
     index[1] = index[0] + 1;
     mx_strdel(&env);
     return true;
 }
 
-bool mx_check_trimmed_str(char *sub_trimmed, char *sub) {
-    bool retval = strlen(sub_trimmed) == 0;
-
-    mx_strdel(&sub_trimmed);
-    if (retval)
+bool mx_check_trimmed_str(char *sub_trim, char *sub) {
+    bool rval = strlen(sub_trim) == 0;
+    mx_strdel(&sub_trim);
+    if (rval) {
         mx_strdel(&sub);
-    return retval;
+    }
+    return rval;
 }
 
-void mx_inc_val_var(unsigned int *len, unsigned int add, char *var) {
-    *len += add;
+void mx_inc_val_var(unsigned int *length, unsigned int add, char *var) {
+    *length += add;
     mx_strdel(&var);
 }
 
-void mx_skip_quotes_if(bool *is_quotes, char *arg, unsigned int *i) {
-    if (arg[*i] == MX_D_QUOTES && !mx_isescape_char(arg, *i))
-        *is_quotes = !*is_quotes;
-    if (!*is_quotes)
+void mx_skip_quotes_if(bool *quote, char *arg, unsigned int *i) {
+    if (arg[*i] == MX_D_QUOTES && !mx_isescape_char(arg, *i)) {
+        *quote = !*quote;
+    }
+    if (!*quote) {
         mx_skip_quotes(arg, i, MX_S_QUOTES);
+    }
 }
