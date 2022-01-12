d4e4affd44e984a66b9417c0265f01ca9a2fab08
Rusewww <rusewww1@gmail.com>
2022-01-12 17:25:52 +0200

    fix

--- src/mx_check_chars.c
+++ src/mx_check_chars.c
@@ -1,15 +1,19 @@
 #include "ush.h"
 
-bool mx_check_chars(char *command) {
-    for (unsigned int i = 0; i < strlen(command); i++) {
-        mx_skip_quotes(command, &i, MX_D_QUOTES);
-        mx_skip_quotes(command, &i, MX_S_QUOTES);
-        for (unsigned int j = 0; j < strlen(MX_FORBIDDEN_CHARS); j++) {
-            if (command[i] == MX_FORBIDDEN_CHARS[j]
-                && !mx_isescape_char(command, i)) {
+bool mx_check_chars(char *cmnd) {
+    unsigned int i = 0;
+    while (i < strlen(cmnd)) {
+        mx_skip_quotes(cmnd, &i, MX_D_QUOTES);
+        mx_skip_quotes(cmnd, &i, MX_S_QUOTES);
+        unsigned int j = 0;
+        while (j < strlen(MX_FORBIDDEN_CHARS)) {
+            if (cmnd[i] == MX_FORBIDDEN_CHARS[j]
+                && !mx_isescape_char(cmnd, i)) {
                 return false;
             }
+            j++;
         }
+        i++;
     }
     return true;
 }
