432458b56c0d3d1c8d1f7a15b708a17a8a4ecb27
Rusewww <rusewww1@gmail.com>
2022-01-12 23:08:19 +0200

    fix

--- src/mx_replace_sub.c
+++ src/mx_replace_sub.c
@@ -5,6 +5,7 @@ char *mx_get_output_fd(int fd) {
     size_t bytes = 0;
     char *out = NULL;
     char buff[128];
+
     while ((bytes = read(fd, buff, sizeof(buff) - 1)) > 0) {
         buff[bytes] = '\0';
         char *tmp = mx_strjoin(*out, buff);
@@ -35,49 +36,47 @@ static char *get_output(char **arg, unsigned int len) {
     return out;
 }
 
-static void parse_commands(char **cmnd, char *sub, char *arg, int *code) {
-    char **arg = NULL;
+static void parse_commands(char **commands, char *sub, char *arg, int *code) {
+    char **arguments = NULL;
     char *out = NULL;
     unsigned int i = 0;
-    while (cmnd[i]) {
-        if (!(arg = mx_interpretate(cmnd[i], code))) {
-            mx_del_strarr(&cmnd);
+    while (commands[i]) {
+        if (!(arguments = mx_interpretate(commands[i], code))) {
+            mx_del_strarr(&commands);
             mx_strdel(&sub);
             continue;
         }
-        out = get_output(arg, strlen(arg));
+        out = get_output(arguments, strlen(arg));
         if (out) {
             strcat(arg, out);
             mx_strdel(&out);
         }
-        mx_del_strarr(&arg);
+        mx_del_strarr(&arguments);
         i++;
     }
 }
 
 bool mx_get_sub(char *arg, char *sub, int *code) {
-    char **cmnd = NULL;
+    char **commands = NULL;
     bool sub_t = true;
     char *sub_tr = NULL;
-
     if (sub[0] == '$') {
         sub_t = false;
     }
     if (mx_remove_subchar(sub)) {
         sub_tr = mx_strtrim(sub);
-        if (mx_check_trimmed_str(sub_tr, sub)) {
+        if (mx_check_trimmed_str(sub_tr, sub))
             return true;
-        }
-        cmnd = mx_parse_command(sub, code);
+        commands = mx_parse_command(sub, code);
     }
-    if (*code || !cmnd) {
+    if (*code || !commands) {
         return false;
     }
     if (sub_t) {
-        mx_replace_sub_escapes(cmnd);
+        mx_replace_sub_escapes(commands);
     }
-    parse_commands(cmnd, sub, arg, code);
+    parse_commands(commands, sub, arg, code);
     mx_strdel(&sub);
-    mx_del_strarr(&cmnd);
+    mx_del_strarr(&commands);
     return true;
 }
