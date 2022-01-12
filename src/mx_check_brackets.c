ca797fb17b59e4c9bd8e50c9a1cfe1428ff788d8
Rusewww <rusewww1@gmail.com>
2022-01-12 17:13:05 +0200

    fix

--- src/mx_check_brackets.c
+++ src/mx_check_brackets.c
@@ -1,22 +1,22 @@
 #include "ush.h"
 
-bool mx_check_brackets(char *c) {
-    unsigned int len = strlen(c);
+bool mx_check_brackets(char *char) {
+    unsigned int len = strlen(char);
     int stack[len];
     int top = -1;
 
     for (unsigned int i = 0; i < len; i++) {
-        mx_skip_quotes(c, &i, MX_GRAVE_ACCENT);
-        if ((c[i] == '(' && !mx_isescape_char(c, i))
-            || (c[i] == '{' && !mx_isescape_char(c, i))
-            || (c[i] == ')' && !mx_isescape_char(c, i))
-            || (c[i] == '}' && !mx_isescape_char(c, i))) {
+        mx_skip_quotes(char, &i, MX_GRAVE_ACCENT);
+        if ((char[i] == '(' && !mx_isescape_char(char, i))
+            || (char[i] == '{' && !mx_isescape_char(char, i))
+            || (char[i] == ')' && !mx_isescape_char(char, i))
+            || (char[i] == '}' && !mx_isescape_char(char, i))) {
             top++;
-            stack[top] = c[i];
+            stack[top] = char[i];
         }
         if ((stack[top] == ')' && (top - 1 >= 0 && stack[top - 1] == '('))
             || (stack[top] == '}' && (top - 1 >= 0
-            && stack[top - 1] == '{'))) {
+                                      && stack[top - 1] == '{'))) {
             top = top - 2;
         }
     }
