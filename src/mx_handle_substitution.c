368454dcc604cf4d776c743ebac89ab28e539ba2
Rusewww <rusewww1@gmail.com>
2022-01-12 22:10:46 +0200

    handle

--- src/mx_handle_substitution.c
+++ src/mx_handle_substitution.c
@@ -3,7 +3,7 @@
 t_list *mx_handle_substitution(t_list *arg) {
     t_list *res = NULL;
 
-    for (; arg; arg = arg->next;) {
+    for (; arg; arg = arg->next) {
         mx_push_back(&res, strdup(arg->data));
     }
     return res;
