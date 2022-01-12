bdd2e45850cfb9e71daa64550ecbfd0a0196d3ff
Rusewww <rusewww1@gmail.com>
2022-01-12 18:49:52 +0200

    proc

--- src/mx_env.c
+++ src/mx_env.c
@@ -1,6 +1,6 @@
 #include "ush.h"
 
-static void env_deinit(char ***env, char **path, char **name) {
+static void env_deinit(char ***env, char **path, char **filename) {
     mx_clearenv();
     mx_env_fill(*env);
     if (*env && *env[0]) {
@@ -9,37 +9,37 @@ static void env_deinit(char ***env, char **path, char **name) {
     if (*path) {
         mx_strdel(path);
     }
-    if (*name) {
-        mx_strdel(name);
-    }
+    if (*filename)
+        mx_strdel(filename);
 }
 
-static int exec_process(char *name, char **argv, int fd) {
-    extern char **env;
+static int exec_process(char *filename, char **argv, int fd) {
+    extern char **environ;
     t_process *process = mx_create_process(fd);
-    int rval = mx_env_exec(process, name, argv, env);
+    int retval = mx_env_exec(process, filename, argv, environ);
+
     mx_del_process(&process);
-    return rval;
+    return retval;
 }
 
 int mx_env(char **argv, int fd) {
     char **env = mx_env_copy();
-    char *name = NULL;
+    char *filename = NULL;
     char *path = NULL;
-    int rval = 0;
+    int retval = 0;
     int i = 0;
 
-    if (!(rval = mx_env_parse_flags(argv, &path, &i))) {
+    if (!(retval = mx_env_parse_flags(argv, &path, &i))) {
         mx_env_parse_vars(argv, &path, &i);
-        if (argv[i] == NULL) {
+        if (argv[i] == NULL)
             mx_print_env(fd);
-        } else if (mx_find_command(path, argv[i], &name)) {
-            rval = exec_process(name, &argv[i], fd);
-        } else {
+        else if (mx_find_command(path, argv[i], &filename))
+            retval = exec_process(filename, &argv[i], fd);
+        else {
             fprintf(stderr, "env: %s: No such file or directory\n", argv[i]);
-            rval = 127;
+            retval = 127;
         }
     }
-    env_deinit(&env, &path, &name);
-    return rval;
+    env_deinit(&env, &path, &filename);
+    return retval;
 }
