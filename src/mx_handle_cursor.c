5bca3e9b0c51bd173857adf004b89eb772d468f4
Rusewww <rusewww1@gmail.com>
2022-01-12 21:19:05 +0200

    getters

--- src/mx_handle_cursor.c
+++ src/mx_handle_cursor.c
@@ -1,22 +1,26 @@
 #include "ush.h"
 
-static void set_cursor(t_prompt *prompt) {
-    unsigned int move_left = prompt->index - prompt->cursor_index;
-
-    for (unsigned int i = 0; i < move_left; i++)
-        printf("\b");
-}
-
 void mx_handle_cursor(t_prompt *prompt) {
-    if (!strcmp(prompt->buff, MX_RIGHT_ARROW))
-        if (prompt->cursor_index < prompt->index)
+    unsigned int left = prompt->index - prompt->cursor_index;
+    if (!strcmp(prompt->buff, MX_RIGHT_ARROW)) {
+        if (prompt->cursor_index < prompt->index) {
             prompt->cursor_index++;
-    if (!strcmp(prompt->buff, MX_LEFT_ARROW))
-        if (prompt->cursor_index)
+        }
+    }
+    if (!strcmp(prompt->buff, MX_LEFT_ARROW)) {
+        if (prompt->cursor_index) {
             prompt->cursor_index--;
-    if (!strcmp(prompt->buff, MX_END_KEY))
+        }
+    }
+    if (!strcmp(prompt->buff, MX_END_KEY)) {
         prompt->cursor_index = prompt->index;
-    if (!strcmp(prompt->buff, MX_HOME_KEY))
+    }
+    if (!strcmp(prompt->buff, MX_HOME_KEY)) {
         prompt->cursor_index = 0;
-    set_cursor(prompt);
+    }
+    unsigned int i = 0;
+    while (i < left) {
+        printf("\b");
+        i++;
+    }
 }
