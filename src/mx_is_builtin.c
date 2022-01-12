76204025ae72c545afc3893ad409a31927f37dea
Rusewww <rusewww1@gmail.com>
2022-01-12 22:22:35 +0200

    handle

--- src/mx_is_builtin.c
+++ src/mx_is_builtin.c
@@ -4,19 +4,13 @@ bool mx_is_builtin(char *command) {
     if (!strcmp(command, "pwd")
         || !strcmp(command, "cd")
         || !strcmp(command, "fg")
-        || !strcmp(command, "set")
         || !strcmp(command, "env")
-        || !strcmp(command, "true")
         || !strcmp(command, "echo")
-        || !strcmp(command, "jobs")
         || !strcmp(command, "exit")
         || !strcmp(command, "unset")
         || !strcmp(command, "color")
-        || !strcmp(command, "false")
         || !strcmp(command, "which")
-        || !strcmp(command, "chdir")
-        || !strcmp(command, "export")
-        || !strcmp(command, "bye")) {
+        || !strcmp(command, "export") {
         return 1;
     }
     return 0;
