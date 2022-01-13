63350cbe00c70faf0c14dbe0c46896f9ea45d1ed
Rusewww <rusewww1@gmail.com>
2022-01-13 15:32:19 +0200

    fix

--- src/mx_pwd.c
+++ src/mx_pwd.c
@@ -50,10 +50,10 @@ void mx_put_pwd(char *pwd, char *old_pwd) {
     chdir(pwd);
     mx_var_list_insert(SHELL, new_pwd);
     mx_var_list_insert(EXP, new_pwd);
-    mx_putenv(new_pwd);
+    mx_put_env(new_pwd);
     mx_var_list_insert(SHELL, remake_pwd);
     mx_var_list_insert(EXP, remake_pwd);
-    mx_putenv(remake_pwd);
+    mx_put_env(remake_pwd);
     mx_strdel(&new_pwd);
     new_pwd = NULL;
     mx_strdel(&remake_pwd);
