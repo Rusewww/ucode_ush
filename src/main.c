1500a179f36830f979d181b76a3bcdc03df22858
Rusewww <rusewww1@gmail.com>
2022-01-13 11:56:41 +0200

    fixush

--- src/main.c
+++ src/main.c
@@ -1,25 +1,5 @@
 #include "../inc/ush.h"
 
-static void main_cycle(void) {
-    int res = 0;
-    t_prompt *prompt = malloc(sizeof(t_prompt));
-    extern char **environ;
-    int fd = isatty(1) ? 1 : 2;
-    if (!isatty(1) && !isatty(2)) {
-        fd = open("/dev/null", O_RDONLY);
-    }
-    while (res != -1) {
-        mx_get_input(prompt, fd, &res);
-        dprintf(fd, "\r\n");
-        mx_handle_command(prompt->command, &res);
-    }
-    if (fd != 1 && fd != 2) {
-        close(fd);
-    }
-    mx_d_del_list(&prompt->history_head);
-    free(prompt);
-}
-
 int main(int argc, char **argv) {
     int exit_code = 0;
     if (argc > 1) {
@@ -32,7 +12,23 @@ int main(int argc, char **argv) {
         mx_deinit();
         exit(exit_code);
     }
-    main_cycle();
+    int res = 0;
+    t_prompt *prompt = malloc(sizeof(t_prompt));
+    extern char **environ;
+    int fd = isatty(1) ? 1 : 2;
+    if (!isatty(1) && !isatty(2)) {
+        fd = open("/dev/null", O_RDONLY);
+    }
+    while (res != -1) {
+        mx_get_in(prompt, fd, &res);
+        dprintf(fd, "\r\n");
+        mx_handle_command(prompt->command, &res);
+    }
+    if (fd != 1 && fd != 2) {
+        close(fd);
+    }
+    mx_d_del_list(&prompt->history_head);
+    free(prompt);
     mx_deinit();
     return 0;
 }
