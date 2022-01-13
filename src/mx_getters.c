36f316c51bec003f89fe4560b19f903cb4ea994d
Rusewww <rusewww1@gmail.com>
2022-01-13 11:53:12 +0200

    geterr1

--- src/mx_getters.c
+++ src/mx_getters.c
@@ -1,26 +1,6 @@
 #include "../inc/ush.h"
 
-/*static void set_code(t_prompt *prompt, int *code) {
-    if (prompt->buff[0] == '\x03' && strlen(prompt->buff) == 1) {
-        memset(prompt->command, '\0', sizeof(prompt->command));
-        memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
-        prompt->index = 0;
-        prompt->cursor_index = 0;
-        prompt->end = false;
-        *code = 130;
-    }
-    if (prompt->buff[0] == '\x04' && !prompt->index && strlen(prompt->buff) == 1) {
-        *code = -1;
-    }
-}
-
-static void clear_view(t_prompt *prompt, int fd) {
-    mx_backspace(prompt->index + strlen(mx_str_prompt()), fd);
-    dprintf(fd, "%s%s", mx_str_prompt(), prompt->command);
-    mx_backspace(prompt->index + strlen(mx_str_prompt()), fd);
-}*/
-
-static bool handle_key(t_prompt *prompt, int fd, int *code) {
+static bool handle(t_prompt *prompt, int fd, int *code) {
     if (prompt->buff[0] == '\x03' && strlen(prompt->buff) == 1) {
         memset(prompt->command, '\0', sizeof(prompt->command));
         memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
@@ -48,14 +28,14 @@ static bool handle_key(t_prompt *prompt, int fd, int *code) {
     return true;
 }
 
-void mx_get_input(t_prompt *prompt, int fd, int *code) {
+void mx_get_in(t_prompt *prompt, int fd, int *code) {
     memset(prompt->command, '\0', sizeof(prompt->command));
     memset(prompt->tmp_command, '\0', sizeof(prompt->tmp_command));
     prompt->index = 0;
     prompt->cursor_index = 0;
     prompt->end = false;
     dprintf(fd, "%s%s", mx_str_prompt(), prompt->command);
-    while (read(STDIN_FILENO, prompt->buff, 4) && prompt->index < ARG_MAX && handle_key(prompt, fd, code)) {
+    while (read(STDIN_FILENO, prompt->buff, 4) && prompt->index < ARG_MAX && handle(prompt, fd, code)) {
         dprintf(fd, "%s%s", mx_str_prompt(), prompt->command);
         mx_handle_cursor(prompt);
         memset(prompt->buff, '\0', sizeof(prompt->buff));
